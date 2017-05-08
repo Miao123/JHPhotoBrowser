133c50b82365cd5fd72c7de4836072be4a3cf4cd
Miao123 <miao_ios@163.comc>
2017-05-08 14:56:27 +0800

    Initial Commit

---
+++ 多张图片显示/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多张图片显示
+//
+//  Created by 苗建浩 on 2017/5/8.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多张图片显示/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多张图片显示
+//
+//  Created by 苗建浩 on 2017/5/8.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
